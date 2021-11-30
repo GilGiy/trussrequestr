35429e610ab5558cb52036493e49b29963617dc1
Gilbert <yodergilbert1@gmail.com>
2021-11-30 13:35:40 -0800

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./src/**/*.html'],
-  },
+  purge: [
+    './resources/views/design.blade.php',
+    './resources/views/welcome.blade.php',
+    './resources/views/layout.blade.php',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
